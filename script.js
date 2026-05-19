935d6459cfd095e472eab437c023aec66ff513a4
Luka Lorch <luka.lorch@gmail.com>
2026-05-19 09:57:12 +0200

    activate gif with new gif

--- script.js
+++ script.js
@@ -85,7 +85,6 @@ function askQuestion() {
     const question = questionInput.value.trim().toLowerCase();
 
     // GIF-Codewort prüfen
-    /*
     if (question === gifCodeword) {
         showGif();
 
@@ -97,7 +96,7 @@ function askQuestion() {
 
         return;
     }
-*/
+    
     // Codewort prüfen
     if (codewordResponses.hasOwnProperty(question)) {
         currentSentence = codewordResponses[question];
